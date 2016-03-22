f422a7efb3c28a994c732afe23ae7ca3cc3ca7b2
samylh1994511 <samylh1994511@gmail.com>
2016-03-22 16:14:51 +0800

    two

---
+++ Proj/app/src/main/java/com/example/sam/proj/WaitActivity.java
@@ -0,0 +1,25 @@
+package com.example.sam.proj;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WaitActivity extends AppCompatActivity {
+    private Handler handler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wait);
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(WaitActivity.this, IndActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}
